04f8977e1d54d7d6bf445d2b983af014c7399119
Cesar-Veiga <cesar.veiga@opendeusto.es>
2025-10-29 11:17:52 +0100

    Cargar el dataset

--- dataCleaningUS.py
+++ dataCleaningUS.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
 
+df = pd.read_csv("US_Accidents_March23.csv")
+pd.set_option('display.max_columns', None)
+
+
+#Some functions to analise the variables
+#df.info()
+#print(df.shape)
+#print(df.describe())
+#print(df.describe(include = object))
